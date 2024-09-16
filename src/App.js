845fd98db3b656d006adcdafd9a5cbc8fad3d5b3
Angelica Pelizza <apelizza@ciandt.com>
2024-09-16 14:06:35 -0300

    feat: use axios and connect on pokeapi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    api.get('pokemon')
+      .then(response => {
+        setPokemons(response.data.results);
+      }).catch(error => {
+        console.log('Algo deu errado!!!', error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokedex</h1>
+      <ul>
+        {pokemons.length> 0 ? (pokemons.map(pokemon => (
+          <li key={pokemon.name}>{pokemon.name}</li>
+        ))) : (
+          <li>Carregando...</li>
+        )}
+      </ul>
     </div>
   );
 }
